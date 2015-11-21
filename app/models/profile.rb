b06390a5bc8344e48b8939ef48bc8c0ba743c07a
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-21 11:44:39 -0800

    Add avatar to profiles database

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,10 @@
 class Profile < ActiveRecord::Base
 
   validates :first_name, :last_name, presence: true
-  
 
   belongs_to :user
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 end
